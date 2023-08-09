1cb53c32ff42e09ee9c0102ba825aab4f2f86366
h0n9 <monsieur.ahn@gmail.com>
2023-08-09 21:05:54 +0900

    Skip relaying msgs to suscribers when full

--- msg/box.go
+++ msg/box.go
@@ -79,6 +79,10 @@ func NewBox(logger *zerolog.Logger, topicID string, topic *pubsub.Topic) (*Box, 
 				return
 			case msgCapsule := <-box.subCh:
 				for _, subscriberCh := range box.subscribers {
+					if len(subscriberCh) == externalChanBufferSize {
+						// when subscriberCh is full, just skip this time
+						continue
+					}
 					subscriberCh <- msgCapsule
 				}
 				msgCapsule = nil // explicitly free
