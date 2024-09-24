4fbe2111a3e9ac0b2515be7d0722fd959107daba
h0n9 <monsieur.ahn@gmail.com>
2024-09-24 21:35:30 +0900

    feat: Prepare flags for loader

--- cli/tools/loader/loader.go
+++ cli/tools/loader/loader.go
@@ -1,6 +1,10 @@
 package loader
 
 import (
+	"fmt"
+	"math/rand"
+	"time"
+
 	"github.com/spf13/cobra"
 )
 
@@ -10,11 +14,28 @@ var Cmd = &cobra.Command{
 	RunE:  runE,
 }
 
+var (
+	tlsEnabled bool
+	hostAddr   string
+	topicID    string
+	nickname   string
+
+	intervalStr string // "1s"
+	loadCount   int    // 0 means unlimited
+)
+
 func runE(cmd *cobra.Command, args []string) error {
 	return nil
 }
 
 func init() {
-	cobra.EnableCommandSorting = false
-	Cmd.AddCommand()
+	r := rand.New(rand.NewSource(time.Now().UnixMicro())).Int()
+
+	Cmd.Flags().BoolVarP(&tlsEnabled, "tls", "t", false, "enable tls connection")
+	Cmd.Flags().StringVar(&hostAddr, "host", "localhost:8080", "host addr")
+	Cmd.Flags().StringVar(&topicID, "topic", "life is beautiful", "topic id")
+	Cmd.Flags().StringVarP(&nickname, "nickname", "n", fmt.Sprintf("alien-%d", r), "consumer id")
+
+	Cmd.Flags().StringVar(&intervalStr, "interval", "1s", "interval")
+	Cmd.Flags().IntVarP(&loadCount, "count", "c", 100, "load count")
 }
