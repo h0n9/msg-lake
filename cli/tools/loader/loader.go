879a31e29eae7d0eecce1f95788ca1d6c65e0355
h0n9 <monsieur.ahn@gmail.com>
2024-09-25 21:04:57 +0900

    feat: Pre-implement skeleton for runE func

--- cli/tools/loader/loader.go
+++ cli/tools/loader/loader.go
@@ -1,11 +1,18 @@
 package loader
 
 import (
+	"context"
 	"fmt"
 	"math/rand"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 
 	"github.com/spf13/cobra"
+
+	"github.com/h0n9/msg-lake/client"
 )
 
 var Cmd = &cobra.Command{
@@ -25,6 +32,39 @@ var (
 )
 
 func runE(cmd *cobra.Command, args []string) error {
+	var msgLakeClient *client.Client
+
+	// init wg
+	wg := sync.WaitGroup{}
+
+	// init sig channel
+	sigCh := make(chan os.Signal, 1)
+	defer close(sigCh)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+	// init ctx with cancel
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// listen signals
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		select {
+		case <-ctx.Done():
+			return
+		case s := <-sigCh:
+			fmt.Printf("got signal %v, attempting graceful shutdown\n", s)
+			if msgLakeClient != nil {
+				msgLakeClient.Close()
+			}
+			fmt.Printf("cancelling ctx ... ")
+			cancel()
+			fmt.Printf("done\n")
+		}
+	}()
+
+	wg.Wait()
+
 	return nil
 }
 
